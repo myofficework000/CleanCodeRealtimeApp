3a5cd86f269a16d02dea807fe3619353d50a769b
myofficework000 <cheetahmail007@gmail.com>
2024-05-08 00:01:19 +0530

    inital commmit

---
+++ app/src/main/java/com/android/cleancoderealtime/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.android.cleancoderealtime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android.cleancoderealtime.ui.theme.CleanCodeRealtimeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleanCodeRealtimeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun HomeScreen(name: String, modifier: Modifier) {
+
+    }
+}
